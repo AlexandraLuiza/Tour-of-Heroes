0a5edbd338c4eb30868d0c7e7e851ecc7fa09445
LUIZA\malex <m.alexandraluiza@gmail.com>
2020-12-26 18:10:19 +0200

    1st assigment ( CSS styling - Modificati layout-ul paginii astfel incat sa afisati continutul pe 3 coloane:  Heroes List  |  Hero Detail  |  Messages  Incercati sa obtineti 3 coloane egale care sa asigure un overview direct al celor 3 sectiuni de continut (cum e exemplul de mai sus) si sa va ganditi unde doriti sa aplicati modificarile. Puteti aborda o solutie pe stiluri globale sau puteti sa scrieti stiluri izolate. De asemenea, puteti modifica template-ul componentelor (HTML).

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,14 @@
+import { NumberValueAccessor } from "@angular/forms";
+
 export interface Hero {
     id: number;
     name: string;
+    // powerStats: {
+    //     combat: number,
+    //     speed: Number,
+    //     intelligence: number,
+    //     strength: number,
+    //     dexterity: number,
+    // }
 }
+
